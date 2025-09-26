95ffaaa46dd119d1c56532da4cba469d8c2e5fc8
virk <144906533+Tavirk@users.noreply.github.com>
2025-09-26 10:12:28 +0530

    Add files via upload

---
+++ News.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+
+const newsSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  category: { 
+    type: String, 
+    enum: ["Political", "Entertainment", "Geographical", "Sports"],
+    required: true 
+  },
+  createdAt: { type: Date, default: Date.now }
+});
+
+module.exports = mongoose.model('News', newsSchema);
